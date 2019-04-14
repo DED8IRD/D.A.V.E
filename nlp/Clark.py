2fecea50817e7620cd15da108c99d562096cae5a
DED8IRD <ewu72829@gmail.com>
2019-04-13 21:45:00 -0700

    leading whitespace not working

--- nlp/Clark.py
+++ nlp/Clark.py
@@ -1,9 +1,12 @@
 # Clark.py
 """
-Generates tab formatted screenplays
+Generates formatted screenplays
 """
 
 class Clark:
+    """
+    Clark generates whitespace formatted screenplays as plaintext files
+    """
     LEFT_MARGIN = 15
     RIGHT_MARGIN = 10
     MAX_WIDTH = 59
@@ -13,30 +16,71 @@ class Clark:
         self.title = title
         self.author = author
         self.pages = []
-        self.current_page = ''
+        self.current_page = []
         self.line_count = 0
 
-    def format_margins(self, text, 
-                       left=self.LEFT_MARGIN, 
-                       right=self.RIGHT_MARGIN, 
-                       max_width=self.MAX_WIDTH, 
-                       align='LEFT', pagewidth=79):
+
+        title_margin = (self.MAX_WIDTH - len(self.title)) // 2
+        screenplay_margin = (self.MAX_WIDTH - len('Screenplay')) // 2
+        author_margin = (self.MAX_WIDTH - len(self.author)) // 2
+
+        title_page = '\n\n'.join(['\n\n',
+                         (' '*title_margin + self.title),
+                         (' '*screenplay_margin + 'Screenplay'),
+                         (' '*((self.MAX_WIDTH - 2) // 2) + 'By'),
+                         (' '*author_margin + self.author + '\n'*8)
+                     ])
+
+        self.pages.append(title_page) 
+
+
+    def __format_line(self, line, align, left, right, page_width):
+        """
+        formats line to page
+
+        """
+        if align == 'LEFT':
+            line = ' '*left + line
+        else:
+            left_pad = page_width - right - len(line)
+            line = ' '*left_pad + line
+        self.current_page.append(line)
+        self.line_count += 1            
+
+    def format(self, text, left=LEFT_MARGIN, right=RIGHT_MARGIN, 
+               max_width=MAX_WIDTH, align='LEFT', page_width=79):
         """
         returns space padded lines
         """
         tokens = text.split()
         remaining_tokens = len(tokens)
         remaining_width = max_width
-        lines = []
-        current_line = ''
+        current_line = '\n'
 
         for token in tokens:
             token_length = len(token)
+
             if remaining_width - token_length > 0:
-                current_line += token
-                remaining_width -= token_length
+                current_line += token + ' '
+                remaining_width -= token_length + 1
             else:
-                current_line = 
-                '\n' + token 
-                remaining_width = max_width - token_length
-                line_count += 1
+                self.__format_line(current_line, align, left, right, page_width)
+                current_line = token + ' '
+                remaining_width = max_width - len(current_line)
+
+                # create new page
+                if self.line_count % self.MAX_LENGTH == 0:
+                    self.pages.append('\n'.join(self.current_page))
+                    self.current_page = [' '*(self.MAX_WIDTH - 5) + str(len(self.pages))]
+        self.__format_line(current_line, align, left, right, page_width)
+
+    def write(self, path):
+        """
+        writes screenplay to filepath
+        """
+        self.pages.append('\n'.join(self.current_page))
+        pages = '\n'.join(self.pages)
+
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(pages)
+            
