25203d6f7447c75514024be0b198e7318b57eb2c
DED8IRD <ewu72829@gmail.com>
2019-04-13 21:45:49 -0700

    format and write to disk

--- nlp/Stanley.py
+++ nlp/Stanley.py
@@ -1,9 +1,14 @@
 # Stanley.py
+"""
+Directs bot generated screenplays
+"""
 
+import json
 import os
+import time
 from random import choice
-from HAL import HAL
-from Clark import Clark
+from HAL import HAL # markov model generator
+from Clark import Clark # text formatter
 
 class Stanley:
     """
@@ -15,19 +20,20 @@ class Stanley:
     ACTIONS = 'ACTIONS'
 
     def __init__(self, genres, characters, 
-                 directory='.', title='Untitled', author='Anonymous'):
+                directory='.', title='Untitled', author='Anonymous'):
         """
         Initializes HAL objects by deserializing Markov models
 
         :genres: list : genre(s) for Markov models
         :characters: list : list of characters for screenplay 
         """
+        start = time.time()
         genres = [genre.upper() for genre in genres]
         self.title = title
         self.author = author
         self.directory = directory
         self.headings = HAL(*self.__filenames(self.HEADINGS, genres))
-        self.parentheticals = HAL(*self.__filenames(self.PARENTHETICALS, genres))
+        self.parentheticals = []
         self.dialogue = HAL(*self.__filenames(self.DIALOGUE, genres))
         self.actions = HAL(*self.__filenames(self.ACTIONS, genres))
         self.characters = [char.upper() for char in characters]
@@ -36,7 +42,9 @@ class Stanley:
             'PAN IN:', 'PAN OUT:', 'DISSOLVE TO:', 'FLASH CUT:', 'SMASH CUT:', 
             'TIME CUT:', 'MATCH CUT:'
         ]
-        self.writer = Clark()
+        self.__get_parentheticals(self.__filenames(self.PARENTHETICALS, genres))
+        self.writer = Clark(title, author)
+        print(f'Completed model rehydration in {time.time() - start} s.')
 
     def __filenames(self, category, genres):
         """
@@ -46,6 +54,17 @@ class Stanley:
         """
         return [os.path.join(self.directory, f'{category}_{genre}.json')
                 for genre in genres]
+
+    def __get_parentheticals(self, files):
+        """
+        loads parentheticals from json
+        """
+        for file in files:
+            if type(file) is str and file.endswith('.json'):
+                # join json
+                with open(file) as f:
+                    model = json.loads(f.read())
+                self.parentheticals += model
 
     def generate(self, attr, max_length=None):
         """
@@ -98,9 +117,19 @@ class Stanley:
         """
         generate tab formatted screenplay
         """
-        HEADINGS_MARGIN = (15, 10, 59, 'LEFT')
+        HEADING_MARGIN = (15, 10, 59, 'LEFT')
         CHARACTER_MARGIN = (42, 10, 33, 'LEFT')
         PARENTHETICAL_MARGIN = (36, 29, 20, 'LEFT')
         DIALOGUE_MARGIN = (29, 23, 33, 'LEFT')
         ACTION_MARGIN = (15, 10, 59, 'LEFT')
         TRANSITION_MARGIN = (59, 10, 15, 'RIGHT')
+
+        for _ in range(100):
+            self.writer.format(self.generate_transition(), *TRANSITION_MARGIN)
+            self.writer.format(self.generate_heading(), *HEADING_MARGIN)
+            self.writer.format(self.generate_action(), *ACTION_MARGIN)
+            self.writer.format(self.generate_character(), *CHARACTER_MARGIN)
+            self.writer.format(self.generate_parenthetical(), *PARENTHETICAL_MARGIN)
+            self.writer.format(self.generate_dialogue(), *DIALOGUE_MARGIN)
+
+        self.writer.write(self.title + '.txt')
