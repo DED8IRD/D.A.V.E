2038a2a3d81dc7e39ec95668af93011d52aa5756
DED8IRD <ewu72829@gmail.com>
2019-08-31 16:29:15 -0700

    refactor package structure

--- DAVE/server/screenplay_generator/urls.py
+++ DAVE/server/screenplay_generator/urls.py
@@ -1,9 +1,11 @@
 """screenplay_generator url config
 """
-from django.urls import re_path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
+from . import views
 
 app_name = 'gen'
 urlpatterns = [
     re_path('.*', TemplateView.as_view(template_name='index.html'), name='client'),
+    path('screenwrite/', views.screenwrite, name='screenwrite'),
 ]
--- DAVE/server/screenplay_generator/views.py
+++ DAVE/server/screenplay_generator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from ...nlp.Stanley import Stanley as Director
+
+def screenwrite(request):
+    genres = ['western']
+    characters = ['bob', 'bobby', 'bobra', 'bobert']
+    source = '../../nlp/markov_models'    
+    director = Director(genres, characters, source)
+    director.direct()
+    return HttpResponse('ok')
