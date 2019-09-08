cf306e8db392124e5d59704f2c9d1815c9d5a4c6
DED8IRD <ewu72829@gmail.com>
2019-09-08 02:16:18 -0700

    update dist

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="DAVE_bot_DED8IRD",
-    version="1.0.6",
+    version="1.0.7",
     author="Eunika Wu",
     author_email="eunika@pdxcodeguild.com",
     description="A screenplay bot",
